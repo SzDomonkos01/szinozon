510c3710989267e1a8bbb248475ccffb552a87aa
SzDomonkos01 <100135163+SzDomonkos01@users.noreply.github.com>
2023-12-18 09:17:59 +0100

    Add files via upload

---
+++ password/password/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+var jelszo = string.Empty;
+Console.Write("Adjon meg a színeket: ");
+ConsoleKey key;
+do
+{
+    var keyInfo = Console.ReadKey(intercept: true);
+    key = keyInfo.Key;
+
+    if (key == ConsoleKey.Backspace && jelszo.Length > 0)
+    {
+        Console.Write("\b \b"); // Így működik
+        jelszo = jelszo[0..^1]; // a törlés!
+    }
+    else if (!char.IsControl(keyInfo.KeyChar))
+    {
+        Console.Write("*");
+        jelszo += keyInfo.KeyChar;
+    }
+} while (key != ConsoleKey.Enter);
+
+
+Console.WriteLine("\nA színek: " + jelszo); // Csak, hogy lássam működik-e!!!
