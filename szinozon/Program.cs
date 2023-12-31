982fb6f8240cddc4bf30a54d191b7271a275fb5a
Lorino <nemeth.lorin.dorina@students.jedlik.eu>
2023-12-31 17:59:14 +0100

    Lorin része alul kommmentbe

--- szinozon/Program.cs
+++ szinozon/Program.cs
@@ -82,3 +82,97 @@ void Main()
 }
 
 Main();
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("SZÍNÖZÖN");
+//Console.WriteLine("1 vagy 2 játekos mód?(1/2)");
+//string hany = Console.ReadLine();
+//string[] ketszinek = new string[4];
+//string[] kettippek = new string[4];
+//string vagaselott = "";
+//int joszin = 0;
+//int mindenjo = 0;
+//int tokeletes = 0;
+//if (int.Parse(hany) == 1)
+//{
+
+//}
+//else
+//{
+//    Console.WriteLine("Adja meg a 4 színt:");
+
+//    for (int i = 0; i < ketszinek.Length; i++)
+//    {
+//        Console.WriteLine($"{i + 1}. szín: ");
+
+//        var jelszo = string.Empty;
+//        ConsoleKey key;
+//        do
+//        {
+//            var keyInfo = Console.ReadKey(intercept: true);
+//            key = keyInfo.Key;
+
+//            if (key == ConsoleKey.Backspace && jelszo.Length > 0)
+//            {
+//                Console.Write("\b \b"); // Így működik
+//                jelszo = jelszo[0..^1]; // a törlés!
+//            }
+//            else if (!char.IsControl(keyInfo.KeyChar))
+//            {
+//                Console.Write("*");
+//                jelszo += keyInfo.KeyChar;
+//            }
+//        } while (key != ConsoleKey.Enter);
+//        ketszinek[i] = jelszo;
+//        Console.WriteLine();
+
+//    }
+//    List<string> vagas = new List<string>();
+
+
+//    do
+//    {
+//        vagas.Clear();
+//        mindenjo = 0;
+//        joszin = 0;
+
+//        Console.WriteLine("Adja meg tippjeit(szóközökkel elválasztva): ");
+//        vagaselott = Console.ReadLine();
+
+//        foreach (var item in vagaselott.Split(' '))
+//        {
+//            vagas.Add(item);
+//        }
+
+//        for (int i = 0; i < ketszinek.Length; i++)
+//        {
+//            if (vagas[i] == ketszinek[i])
+//            {
+//                mindenjo += 1;
+//            }
+//            else if (ketszinek.Contains(vagas[i]))
+//            {
+//                joszin += 1;
+//            }
+//        }
+//        Console.WriteLine($"{mindenjo}db van jó helyen");
+//        Console.WriteLine($"{joszin}db van van benne rossz helyen");
+//        Console.WriteLine("\n");
+//    } while (mindenjo != 4);
+//    Console.WriteLine("Gratulálok, eltaláltad az összes színt!");
+
+//    for (int i = 0; i < ketszinek.Length; i++)
+//    {
+//        if (kettippek[i] == ketszinek[i])
+//        {
+//            tokeletes += 1;
+//        }
+//    }
+//    if (tokeletes == 4)
+//    {
+
+//    }
+
+
+
+//}
+
